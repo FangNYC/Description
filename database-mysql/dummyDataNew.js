cbc00678e5f8571df718f77847406ea6f5d50753
Hack Reactor Student <student@hackreactor.com>
2018-11-08 16:39:30 -0500

    async function working; working on speed optimization

--- database-mysql/dummyDataNew.js
+++ database-mysql/dummyDataNew.js
@@ -92,6 +92,258 @@ function Data() {
 // });
 
 
+
+
+
+
+
+
+
+
+
+
+    // setTimeout(() => {
+    // for (var i = 0; i < batchSize; i++) {
+    //   //console.log(i)
+    //   count += 1;
+
+    //   ID = count;
+    //   var data = new Data;
+    //   db.interstAll(ID, data.name, data.roomType, data.roomTypeDetails, data.city, data.cityDetails, data.listingDetails, data.guestAccessDetails, data.interactionGuestsDetails, data.otherDetails, function (err, result) {
+    //     if (err) {
+    //       console.log(err);
+    //     } else {
+    //       console.log('added: ', count)
+    //       //console.log(result);
+    //     }
+    //   });
+    // }
+    // }, 
+
+
+
+
+
+  // var delayInsert = (total) => {
+  //   var count = 0;
+  //   var recurse = () => {
+  //     if (total === count) {
+  //       return;
+  //     } else {
+  //       ID = count;
+  //       var data = new Data;
+  //       db.interstAll(ID, data.name, data.roomType, data.roomTypeDetails, data.city, data.cityDetails, data.listingDetails, data.guestAccessDetails, data.interactionGuestsDetails, data.otherDetails, function (err, result) {
+  //         if (err) {
+  //           console.log(err);
+  //         } else {
+  //           console.log('added: ', count)
+  //           //console.log(result);
+  //         }
+  //       });
+  //       count++;
+  //       setTimeout(recurse(), 0.1)
+  //     }
+  //   }
+  //   recurse();
+  // }
+
+  // delayInsert(10000000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var count = 0;
+// var totalSize = 10000000;
+// var batchSize = 10000;
+// var wrapper = () => {
+//   if(count >= totalSize) {
+//     console.log('count complete: ', count);
+//     return;
+//   }
+//   var promise = new Promise(
+//     function(resolve) {
+//     setTimeout(() => {
+//     for (var i = 0; i < batchSize; i++) {
+//       //console.log(i)
+//       count += 1;
+
+//       ID = count;
+//       var data = new Data;
+//       db.interstAll(ID, data.name, data.roomType, data.roomTypeDetails, data.city, data.cityDetails, data.listingDetails, data.guestAccessDetails, data.interactionGuestsDetails, data.otherDetails, function (err, result) {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           //console.log('added: ', count)
+//           const used = process.memoryUsage().heapUsed / 1024 / 1024;
+//         console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+//           //console.log(result);
+//         }
+//       });
+
+
+
+//       if (i === batchSize-1) {
+//         // console.log('count step: ', count);
+//         // console.log('...now get the next batch')
+//         resolve();
+//       }
+//     }
+//     }, 5000)
+//   });
+
+//   promise.then(function() {
+//     wrapper();
+//   });
+// }
+
+// wrapper();
+
+
+
+
+
+
+
+// var count = 0;
+// var loop = (cb, min, current) => {
+//   setTimeout(() => {
+//     cb();
+//     if (current() < min) {
+//       // console.log('min: ', min)
+//       // console.log('current: ', current())
+//       loop(cb, min, current);
+//     }
+//   }, 1000)
+// }
+
+// // loop(() => {
+// //   count += 1;
+// //   console.log('repeat: ', count)
+// // }, 6, () => { return count; });
+
+
+
+
+// var count = 0;
+// var totalSize = 10000000;
+// var batchSize = 10000;
+// var wrapper = () => {
+//   if(count >= totalSize) {
+//     console.log('count complete: ', count);
+//     return;
+//   }
+//   var promise = new Promise(
+//     function(resolve) {
+//     setTimeout(() => {
+
+//       count += 1;
+
+//       ID = count;
+//       var data = new Data;
+//       db.interstAll(ID, data.name, data.roomType, data.roomTypeDetails, data.city, data.cityDetails, data.listingDetails, data.guestAccessDetails, data.interactionGuestsDetails, data.otherDetails, function (err, result) {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           //console.log('added: ', count)
+//           const used = process.memoryUsage().heapUsed / 1024 / 1024;
+//           console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+
+//           if ((Math.round(used * 100) / 100) > 750) {
+//             console.log('exceeded max memory: ', (Math.round(used * 100) / 100));
+//             loop(() => {
+//               console.log('...waiting to clear memory, current: ', (Math.round(used * 100) / 100));
+//               // if ((Math.round(used * 100) / 100) < 250) {
+//               //   //resolve();
+//               // };
+//             }, 250, () => {
+//               return used;
+//             })
+//           }
+//           resolve();
+//         }
+//       });
+
+//     }, 0)
+//   });
+
+//   promise.then(function() {
+//     wrapper();
+//   });
+// }
+
+// wrapper();
+
+
+
+
+
+
+// var count = 0;
+// var totalSize = 10000000;
+// var batchSize = 10000;
+// var wrapper = () => {
+//   if(count >= totalSize) {
+//     console.log('count complete: ', count);
+//     return;
+//   }
+//   var promise = new Promise(
+//     function(resolve) {
+//     setTimeout(() => {
+//     for (var i = 0; i < batchSize; i++) {
+//       //console.log(i)
+//       count += 1;
+
+//       ID = count;
+//       var data = new Data;
+//       db.interstAll(ID, data.name, data.roomType, data.roomTypeDetails, data.city, data.cityDetails, data.listingDetails, data.guestAccessDetails, data.interactionGuestsDetails, data.otherDetails, function (err, result) {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           //console.log('added: ', count)
+//           const used = process.memoryUsage().heapUsed / 1024 / 1024;
+//         console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+//           //console.log(result);
+//           if (i === batchSize-1) {
+//             console.log('count step: ', count);
+//             console.log('...now get the next batch')
+//             resolve();
+//           }
+//         }
+//       });
+
+//     }
+//     }, 0)
+//   });
+
+//   promise.then(function() {
+//     wrapper();
+//   });
+// }
+
+// wrapper();
+
+
+
+var today = new Date();
+var startMinutes = ((today.getHours() * 60) + today.getMinutes());
+var startSeconds = today.getSeconds();
+
+
 var count = 0;
 var totalSize = 100000;
 var batchSize = 10000;
@@ -102,30 +354,37 @@ var wrapper = () => {
   }
   var promise = new Promise(
     function(resolve) {
-    setTimeout(() => {
-    for (var i = 0; i < batchSize; i++) {
-      console.log(i)
+    //setTimeout(() => {
       count += 1;
-
       ID = count;
       var data = new Data;
       db.interstAll(ID, data.name, data.roomType, data.roomTypeDetails, data.city, data.cityDetails, data.listingDetails, data.guestAccessDetails, data.interactionGuestsDetails, data.otherDetails, function (err, result) {
         if (err) {
           console.log(err);
         } else {
-          console.log(result);
+          // console.log('added: ', count)
+          // const used = process.memoryUsage().heapUsed / 1024 / 1024;
+          // console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+          // if (count === 100 || count === 1000 || count % 10000 === 0 || count === 100000 || count === 1000000) {
+          //   const used = process.memoryUsage().heapUsed / 1024 / 1024;
+          //   console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+          //   console.log('count: ', count);
+          // }
+          if (count === 100000) {
+            console.log ('reached: ', count);
+            var today = new Date();
+
+            var minutes = ((today.getHours() * 60) + today.getMinutes()) - startMinutes;
+            var seconds = today.getSeconds() - startSeconds;
+
+            console.log('time elapsed:', minutes, 'm : ', seconds, 's');
+
+          }
+          resolve();
         }
       });
 
-
-
-      if (i === batchSize-1) {
-        console.log('count step: ', count);
-        console.log('...now get the next batch')
-        resolve();
-      }
-    }
-    }, 0)
+    //}, 0)
   });
 
   promise.then(function() {
